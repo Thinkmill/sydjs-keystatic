05ef4b7b06d4e5749e976e22508186d66ae3af46
Simon Vrachliotis <simon@simonswiss.com>
2023-04-13 16:53:15 +1000

    build passes but i made a mess

--- src/components/event.tsx
+++ src/components/event.tsx
@@ -44,19 +44,26 @@ const eventStatusClasses: Record<EventProps['status'], string> = {
 
 export default function Event({ event }: Props) {
   return (
-    <div className={clsx('rounded-[40px] p-16', eventStatusClasses[event.status])}>
+    <div
+      className={clsx(
+        'rounded-[40px] p-16',
+        // Adding the fallback until I fix the breaking build 😅
+        eventStatusClasses[event?.status] ?? eventStatusClasses.upcoming
+      )}
+    >
+      <pre>{JSON.stringify(event, null, 2)}</pre>
       <span className="rounded-full border-2 border-black px-4 py-1.5 text-sm font-bold leading-none">
         upcoming event
       </span>
 
       <div className="grid gap-28 md:grid-cols-3">
         <div className="md:col-span-2">
-          <h2 className="mt-8 text-4xl font-bold">{event.name}</h2>
+          <h2 className="mt-8 text-4xl font-bold">{event?.name}</h2>
           <div className="mt-4 space-y-4 text-lg">
-            <DocumentRenderer document={event.description} />
+            {event?.description && <DocumentRenderer document={event?.description} />}
           </div>
           <div className="mt-6 flex items-center gap-4">
-            <Button href={`/events/${event.slug}`} size="large">
+            <Button href={`/events/${event?.slug}`} size="large">
               View events details
             </Button>
             <Button
@@ -74,10 +81,10 @@ export default function Event({ event }: Props) {
           {[
             {
               icon: CalendarClearOutlineIcon,
-              text: format(new Date(event.date), 'EEEE, d MMMM yyy'),
+              text: event?.date ? format(new Date(event.date), 'EEEE, d MMMM yyy') : 'no  date',
             },
             { icon: ClockIcon, text: '6:30 - 9:30 pm' },
-            { icon: LocationOutlineIcon, text: event.location },
+            { icon: LocationOutlineIcon, text: event?.location },
             { icon: DesktopIcon, text: 'Online event' },
           ].map((event) => {
             const Icon = event.icon
