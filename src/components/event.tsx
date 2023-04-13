5177fdf8ed42ed411dec6340eaab466d31f6cfd4
Simon Vrachliotis <simon@simonswiss.com>
2023-04-13 21:04:24 +1000

    Tweaks and checks for undefined

--- src/components/event.tsx
+++ src/components/event.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import clsx from 'clsx'
 import { format } from 'date-fns'
 
@@ -43,6 +44,15 @@ const eventStatusClasses: Record<EventProps['status'], string> = {
 }
 
 export default function Event({ event }: Props) {
+  const eventMeta = [
+    {
+      icon: CalendarClearOutlineIcon,
+      text: event?.date ? format(new Date(event.date), 'EEEE, d MMMM yyy') : 'no  date',
+    },
+    { icon: ClockIcon, text: '6:30 - 9:30 pm', secondary: true },
+    { icon: LocationOutlineIcon, text: event?.location },
+    { icon: DesktopIcon, text: 'Online event', secondary: true },
+  ]
   return (
     // We're using container queries here!
     // The wrapped is flagged as a `@container`
@@ -60,7 +70,7 @@ export default function Event({ event }: Props) {
           )}
         >
           <span className="inline-block rounded-full border-2 border-black px-4 py-1.5 text-sm font-bold leading-none">
-            {event.status === 'past' ? 'past' : 'upcoming'} event
+            {event?.status === 'past' ? 'past' : 'upcoming'} event
           </span>
 
           <div className="grid gap-16 md:grid-cols-3 xl:gap-28">
@@ -85,15 +95,7 @@ export default function Event({ event }: Props) {
               </div>
             </div>
             <ul className="space-y-4">
-              {[
-                {
-                  icon: CalendarClearOutlineIcon,
-                  text: event?.date ? format(new Date(event.date), 'EEEE, d MMMM yyy') : 'no  date',
-                },
-                { icon: ClockIcon, text: '6:30 - 9:30 pm' },
-                { icon: LocationOutlineIcon, text: event?.location },
-                { icon: DesktopIcon, text: 'Online event' },
-              ].map((event) => {
+              {eventMeta.map((event) => {
                 const Icon = event.icon
                 return (
                   <li key={event.text} className="flex items-center gap-3">
@@ -120,35 +122,34 @@ export default function Event({ event }: Props) {
             eventStatusClasses[event?.status] ?? eventStatusClasses.upcoming
           )}
         >
-          {event.status !== 'past' && (
+          {event?.status !== 'past' && (
             <span className="mb-4 inline-block rounded-full border-2 border-black px-4 py-1.5 text-sm font-bold leading-none">
               upcoming event
             </span>
           )}
-          <h2 className="text-2xl font-bold">Thinkmill takes over SydJS</h2>
+          <Link href={`/events/${event?.slug}`} className="hover:underline">
+            <h2 className="text-2xl font-bold">{event?.name}</h2>
+          </Link>
           <ul className="mt-6 space-y-4">
-            {[
-              { icon: CalendarClearOutlineIcon, text: 'Monday, 17 April 2023' },
-              { icon: LocationOutlineIcon, text: 'Atlassian Sydney Office' },
-            ].map((event) => {
-              const Icon = event.icon
-              return (
-                <li key={event.text} className="flex items-center gap-3">
-                  <div className="shrink-0 rounded-xl bg-black/10 p-2.5">
-                    <Icon className="h-5 w-5 stroke-black" />
-                  </div>
-                  <span className="text-lg/6 font-medium">{event.text}</span>
-                </li>
-              )
-            })}
+            {eventMeta
+              .filter((event) => !event.secondary)
+              .map((event) => {
+                const Icon = event.icon
+                return (
+                  <li key={event.text} className="flex items-center gap-3">
+                    <div className="shrink-0 rounded-xl bg-black/10 p-2.5">
+                      <Icon className="h-5 w-5 stroke-black" />
+                    </div>
+                    <span className="text-lg/6 font-medium">{event.text}</span>
+                  </li>
+                )
+              })}
           </ul>
           <p className="mt-8 line-clamp-[7] text-lg/6">
-            We know that Open Source Software is a great way to ensure that the best minds get to
-            work on the best solutions to make the best outcomes for all Developers. But what does
-            it take to make a successful Open Source solution?
+            {event?.description && <DocumentRenderer document={event?.description} />}
           </p>
 
-          {event.status !== 'past' && (
+          {event?.status !== 'past' && (
             <div className="mt-8 flex flex-wrap items-center gap-4">
               <Button href={`/events/${event?.slug}`} size="large">
                 View events details
