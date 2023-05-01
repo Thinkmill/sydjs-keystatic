8b463fd49e323878d9f507d87ba871b4886de79c
Joe Prisk <joe.prisk@thinkmill.com.au>
2023-05-01 15:48:38 +1200

    added page title to layout

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,6 +4,18 @@ import { Poppins } from 'next/font/google'
 import Navigation from '@/components/navigation'
 import Footer from '@/components/footer'
 
+import { Metadata } from 'next'
+
+// https://beta.nextjs.org/docs/api-reference/metadata#metadata-object
+export const metadata: Metadata = {
+  title: {
+    default: 'SydJS',
+    template: '%s | SydJS',
+  },
+  description:
+    'Join the vibrant and inclusive community of web developers discussing the latest in Javascript from Sydney, Australia.',
+}
+
 const poppins = Poppins({
   weight: ['400', '500', '600', '700'],
   subsets: ['latin'],
