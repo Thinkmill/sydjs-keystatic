a0c4d3c64b389e4c127fe8e49c0ba83a0cb83363
Joe Prisk <joe.prisk@thinkmill.com.au>
2023-05-03 09:43:09 +1200

    added horizontal scroll to nav

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -13,8 +13,8 @@ export default function Navigation() {
   const [isModalOpen, setIsModalOpen] = useState(false)
   return (
     <>
-      <nav className="relative z-20 mx-auto flex max-w-6xl items-center justify-between px-6 py-4 lg:px-8">
-        <Link href="/">
+      <nav className="relative z-20 mx-auto flex max-w-6xl items-center justify-between overflow-x-scroll px-6 py-4 lg:px-8">
+        <Link className="pr-8" href="/">
           <Logo />
         </Link>
         <ul className="flex items-center gap-8">
@@ -30,10 +30,10 @@ export default function Navigation() {
           </li>
           <li>
             <Button emphasis="high" onClick={() => setIsModalOpen(true)}>
-              Join mailing list
+              <span className="whitespace-nowrap">Join mailing list</span>
             </Button>
           </li>
-          <li className="-ml-4">
+          <li className="-ml-4 hidden md:block">
             <IconButton
               emphasis="low"
               href="https://twitter.com/sydjs"
