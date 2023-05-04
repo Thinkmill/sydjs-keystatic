7f5366ef6d700777bc67d12556051a77394dc349
Simon Vrachliotis <simon@simonswiss.com>
2023-05-04 13:37:09 +1000

    Fix import paths for Keystatic config

--- src/app/keystatic/keystatic.tsx
+++ src/app/keystatic/keystatic.tsx
@@ -1,5 +1,5 @@
 'use client'
 import { makePage } from '@keystatic/next/ui/app'
-import config from '../../../keystatic.config'
+import config from '@/app/keystatic/keystatic.config'
 
 export default makePage(config)
