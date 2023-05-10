7c65a2e1f315dd5b3d1d0a2cafda8c34a62a8e8b
Simon Vrachliotis <simon@simonswiss.com>
2023-05-10 10:26:00 +1000

    Event schema - add descriptions and change order

--- src/app/keystatic/schema/collections/events.ts
+++ src/app/keystatic/schema/collections/events.ts
@@ -8,39 +8,20 @@ export default collection({
     name: fields.slug({
       name: {
         label: 'Name',
+        description: 'The name of the event.',
+      },
+      slug: {
+        description:
+          'The slug for the event. This is auto-generated and will be used in the URL.',
       },
     }),
     description: fields.document({
-      label: 'Description',
+      label: 'Event description',
+      description: 'The copy that will show in the event card.',
       formatting: true,
     }),
-    talks: fields.array(
-      fields.relationship({
-        label: 'Talk',
-        collection: 'talks',
-        validation: { isRequired: true },
-      }),
-      {
-        label: 'Talks',
-        itemLabel: (props) => props.value ?? 'Please select a talk',
-      }
-    ),
-    sponsors: fields.array(
-      fields.relationship({
-        label: 'Sponsor',
-        collection: 'sponsors',
-        validation: { isRequired: true },
-      }),
-      {
-        label: 'Sponsors',
-        itemLabel: (props) => props.value ?? 'Please select a sponsor',
-      }
-    ),
-    date: fields.date({ label: 'Date', validation: { isRequired: true } }),
-    location: fields.text({ label: 'Location' }),
-    address: fields.text({ label: 'Address' }),
-    startTime: fields.text({ label: 'Start time' }),
-    endTime: fields.text({ label: 'End time' }),
+
+    // Featured media (image or video)
     feature: fields.blocks(
       {
         image: {
@@ -64,7 +45,55 @@ export default collection({
           }),
         },
       },
-      { label: 'Featured Media', validation: { length: { max: 1 } } }
+      {
+        label: 'Featured Media',
+        description:
+          'Optional image or video to display as a "hero" featured media on the event details page.',
+        validation: { length: { max: 1 } },
+      }
+    ),
+
+    date: fields.date({ label: 'Date', validation: { isRequired: true } }),
+    location: fields.text({
+      label: 'Location',
+      description: 'Venue name, short. Use address field for details.',
+    }),
+    address: fields.text({ label: 'Address' }),
+    startTime: fields.text({
+      label: 'Start time',
+      description: 'Example: 6:30 PM',
+    }),
+    endTime: fields.text({
+      label: 'End time',
+      description: 'Example: 8:30 PM',
+    }),
+
+    // Relationship to Talks
+    talks: fields.array(
+      fields.relationship({
+        label: 'Talk',
+        collection: 'talks',
+        validation: { isRequired: true },
+      }),
+      {
+        label: 'Talks',
+        description:
+          'The talks (from the Talks collection) that will be presented at this event.',
+        itemLabel: (props) => props.value ?? 'Please select a talk',
+      }
+    ),
+
+    // Relationship to Sponsors
+    sponsors: fields.array(
+      fields.relationship({
+        label: 'Sponsor',
+        collection: 'sponsors',
+        validation: { isRequired: true },
+      }),
+      {
+        label: 'Sponsors',
+        itemLabel: (props) => props.value ?? 'Please select a sponsor',
+      }
     ),
   },
 })
